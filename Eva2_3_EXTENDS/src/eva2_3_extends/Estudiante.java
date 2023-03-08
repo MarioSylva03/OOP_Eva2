7749f922ea14794c3cd203f61323b45f7675648b
MarioSylva03 <124391494+MarioSylva03@users.noreply.github.com>
2023-03-08 02:05:14 -0600

    Add files via upload

---
+++ Eva2_3_EXTENDS/src/eva2_3_extends/Estudiante.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_3_extends;
+
+/**
+ *
+ * @author mario
+ */
+
+// CLASE DERIVADA extends calse base
+// CLASE HIJO extends CLASE PADRE
+//SUBCLASE extends SUPERCLASE
+public class Estudiante extends Persona {
+    private int Ncontrol;
+
+    public int getNcontrol() {
+        return Ncontrol;
+    }
+
+    public void setNcontrol(int Ncontrol) {
+        this.Ncontrol = Ncontrol;
+    }
+    
+    
+}
---
+++ Eva2_3_EXTENDS/src/eva2_3_extends/Eva2_3_EXTENDS.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_3_extends;
+
+/**
+ *
+ * @author mario
+ */
+public class Eva2_3_EXTENDS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiante estu = new Estudiante();
+        estu.setNombre("Mario");
+        estu.setApellido("Sylva");
+        estu.setEdad(19);
+        estu.setNcontrol(22550369);
+        System.out.println("DATOS DEL ESTUDIANTE");
+        System.out.println(estu.getNombre());
+        System.out.println(estu.getApellido());
+        System.out.println(estu.getEdad());
+        System.out.println(estu.getNcontrol());
+    }
+    
+}

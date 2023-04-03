1199524d71dcdfddf3fa3dd298b3c2d570cc47e0
MarioSylva03 <124391494+MarioSylva03@users.noreply.github.com>
2023-04-03 12:43:58 -0600

    Add files via upload

---
+++ Eva2_9_Super/src/Eva2_9_Super/Eva2_9_Super.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eva2_9_Super;
+
+/**
+ *
+ * @author mario
+ */
+public class Eva2_9_Super {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiante estu = new Estudiante("22550369", "Mario", "Sylva Iracheta", 19 );
+        estu.imprimirdatos();
+        
+        Docente doce1 = new Docente();
+        doce1.imprimirdatos();        
+        
+    }
+    
+}

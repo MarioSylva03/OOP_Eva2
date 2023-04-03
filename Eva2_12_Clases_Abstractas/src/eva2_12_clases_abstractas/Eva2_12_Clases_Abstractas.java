95a673ea216f08420254e15b45c1d9542a6a5e1c
MarioSylva03 <124391494+MarioSylva03@users.noreply.github.com>
2023-04-03 12:45:15 -0600

    Add files via upload

---
+++ Eva2_12_Clases_Abstractas/src/eva2_12_clases_abstractas/Eva2_12_Clases_Abstractas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author mario
+ */
+public class Eva2_12_Clases_Abstractas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    prueba P1 = new prueba();    
+    claseabstracta C1 = new claseabstracta();
+    }
+    
+}
+class prueba{
+    
+}
+/*No se puede instanciar
+abstract class claseabstracta{
+    
+}
